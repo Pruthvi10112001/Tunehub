386803d8559a7f1c18e09b4510ae14287dd01900
USER <USER@192.168.100.15>
2024-02-01 15:30:43 +0530

    Music Player

--- src/main/java/com/kodnest/boot01/controller/UsersController.java
+++ src/main/java/com/kodnest/boot01/controller/UsersController.java
@@ -1,13 +1,18 @@
 package com.kodnest.boot01.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.kodnest.boot01.entities.Song;
 import com.kodnest.boot01.entities.Users;
+import com.kodnest.boot01.services.SongService;
 import com.kodnest.boot01.services.UsersService;
 
 import jakarta.servlet.http.HttpSession;
@@ -18,6 +23,9 @@ import jakarta.servlet.http.HttpSession;
 public class UsersController {
 	@Autowired
 	UsersService service;
+	
+	@Autowired
+	SongService songService;
 	@PostMapping("/register")
 	public String addUsers(@ModelAttribute Users user) {
 		boolean userStatus=service.emailExists(user.getEmail());
@@ -33,7 +41,7 @@ public class UsersController {
 	}
 	
 	@PostMapping("/validate")
-	public String validate(@RequestParam("email") String email,@RequestParam("password") String password , HttpSession session) {
+	public String validate(@RequestParam("email") String email,@RequestParam("password") String password , HttpSession session,Model model) {
 		if(service.validateUser(email,password)== true)
 		{
 			String role=service.getRole(email);
@@ -46,6 +54,12 @@ public class UsersController {
 			}
 			else
 			{
+				Users user=service.getUser(email);
+				boolean userStatus=user.isPremium();
+				List<Song> songsList=songService.fetchAllSongs();
+				model.addAttribute("songs", songsList);
+				
+				model.addAttribute("isPremium",userStatus);
 				return "customerHome";
 			}
 		}
